23c8d5d549f56d45c7fbdf98f53055fde0a56b8f
Jacob Rogaishio <me@JacobRogaishio.com>
2014-03-01 18:28:04 -0500

    Fix for post button stating update when creating

--- models/post.php
+++ models/post.php
@@ -196,7 +196,7 @@ class post extends model
 		<div class="clear"></div>
 		<br />
 
-		<input type="submit" name="saveChanges" class="btn btn-success btn-large" value="' . ((!isset($postId) || $postId == null) ? "Create" : "Update") . ' This Post!" /><br /><br />
+		<input type="submit" name="saveChanges" class="btn btn-success btn-large" value="' . ((!isset($postId) || $postId == null || $postId == "new") ? "Create" : "Update") . ' This Post!" /><br /><br />
 		' . ((isset($postId) && $postId != null) ? '<a href="admin.php?type=post&action=delete&p=' . $pageId . '&c=' . $postId . '" class="deleteBtn">Delete This Post!</a><br /><br />' : '') . '
 		</form>
 		<br />
