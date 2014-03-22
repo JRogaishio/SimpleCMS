8f33f08b0f91d14ec1e44c1319ac3a32d54bd630
Jacob Rogaishio <me@JacobRogaishio.com>
2014-03-21 20:41:22 -0400

    Removed unnecessary configuration items and updated folder names

--- config.php
+++ config.php
@@ -2,22 +2,22 @@
 session_start();
 ini_set( "display_errors", true );
 date_default_timezone_set( "America/New_York" );  // http://www.php.net/manual/en/timezones.php
+define( "DB_HOST", "localhost" ); 	//Database host
+define( "DB_NAME", "ferret_cms" ); 	//Database username
+define( "DB_USERNAME", "root" ); 	//Database username
+define( "DB_PASSWORD", "" );		//Database password
+
+//SYSTEM  BELOW - DO NOT EDIT
 define( "SYSTEM_VERSION", file_get_contents('version.txt'));
-define( "DB_HOST", "localhost" );
-define( "DB_NAME", "ferret_cms" );
-define( "DB_USERNAME", "root" );
-define( "DB_PASSWORD", "" );
 define( "CONTOLLER_PATH", "controllers" );
 define( "MODEL_PATH", "models" );
 define( "VIEW_PATH", "views" );
-define( "TEMPLATE_PATH", "template" );
-define( "PLUGIN_PATH", "plugin" );
+define( "TEMPLATE_PATH", "templates" );
+define( "PLUGIN_PATH", "plugins" );
 define( "ADMINFORM_PATH", "adminForm" );
 define( "LIBRARY_PATH", "lib" );
 define( "SITE_ROOT", "http://localhost/ferretCMS/" );
-define( "ADMIN_USERNAME", "admin" );
-define( "ADMIN_PASSWORD", "pass" );
-define( "ERROR_DIR", "template/system/" );
+define( "ERROR_DIR", "templates/system/" );
 define( "DATEFORMAT", "M, j Y" );
 define( "TIMEFORMAT", "h:i:s A" );
 
