fabfdea4d375f5a6d684b7eff500f892a227fde9
Jacob Rogaishio <me@JacobRogaishio.com>
2014-03-21 20:47:57 -0400

    Update config settings

--- config.php
+++ config.php
@@ -1,11 +1,16 @@
 <?php
 session_start();
 ini_set( "display_errors", true );
+
+//SITE CONFIGURATIONS
 date_default_timezone_set( "America/New_York" );  // http://www.php.net/manual/en/timezones.php
 define( "DB_HOST", "localhost" ); 	//Database host
 define( "DB_NAME", "ferret_cms" ); 	//Database username
 define( "DB_USERNAME", "root" ); 	//Database username
 define( "DB_PASSWORD", "" );		//Database password
+define( "SITE_ROOT", "http://localhost/ferretCMS/" ); //This should be http://mywebsite.com/
+define( "DATEFORMAT", "M, j Y" );
+define( "TIMEFORMAT", "h:i:s A" );
 
 //SYSTEM  BELOW - DO NOT EDIT
 define( "SYSTEM_VERSION", file_get_contents('version.txt'));
@@ -16,10 +21,7 @@ define( "TEMPLATE_PATH", "templates" );
 define( "PLUGIN_PATH", "plugins" );
 define( "ADMINFORM_PATH", "adminForm" );
 define( "LIBRARY_PATH", "lib" );
-define( "SITE_ROOT", "http://localhost/ferretCMS/" );
 define( "ERROR_DIR", "templates/system/" );
-define( "DATEFORMAT", "M, j Y" );
-define( "TIMEFORMAT", "h:i:s A" );
 
 function handleException( $exception ) {
   echo "Sorry, a problem occurred. Please try later.";
